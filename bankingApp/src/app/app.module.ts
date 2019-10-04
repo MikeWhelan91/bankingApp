92fa5eeefb1b12a3059810ce520a4efe56cf2189
Michael Whelan <whelano07@hotmail.com>
2019-10-04 14:26:19 +0100

    added components

--- bankingApp/src/app/app.module.ts
+++ bankingApp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SavingsComponent } from './savings/savings.component';
+import { MortgageComponent } from './mortgage/mortgage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SavingsComponent,
+    MortgageComponent
   ],
   imports: [
     BrowserModule,
