36c5c50c94ac2a3908e2399bd350530b88afbfe5
Michael Whelan <whelano07@hotmail.com>
2019-10-04 14:41:01 +0100

    added savings calcs

--- bankingApp/src/app/savings/savings.component.ts
+++ bankingApp/src/app/savings/savings.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-savings',
   templateUrl: './savings.component.html',
   styleUrls: ['./savings.component.css']
 })
-export class SavingsComponent implements OnInit {
+export class SavingsComponent {
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+  Total: number;
+  Amount = 5000;
+  interestRate = 0.01;
+  annualCompound = 1;
+  years: number;
+  calaulateinterest(): void {
+    for (let counter = 1; counter < this.years + 1; counter++) {
+      this.Total = this.Amount * Math.pow((1 +
+        this.interestRate / this.annualCompound),
+        (this.annualCompound * counter));
+      }
+    }
 
 }
